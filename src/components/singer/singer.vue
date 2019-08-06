68aadce4d069f3258991a38ac040c1c263456aad
huqionglu <190109@lifeat.cn>
2019-08-06 20:12:09 +0800

    修改

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,34 +3,52 @@
 </template>
 
 <script type="text/ecmascript-6">
-import {getSingerList} from 'api/singer'
-import {ERR_OK} from 'api/config'
-  
-  export default {
-    data() {
-      return {
-      singers:[]
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
 
-      }
-    },
-    created() {
-      this._getSingerList()
-    },
-    methods:{
-      _getSingerList() {
-        getSingerList().then(res => {
-            if (res.code === ERR_OK ) {
-              this.singers = res.data.list
-              console.log(this.singers)
-            }
-        })
-      }
+export default {
+  data() {
+    return {
+      singers: []
+
     }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data)
+          this.singers = res.data.list
+        }
+      })
+
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push({
+            id: item.Fsinger.mid,
+            name: item.Fsinger.name,
+            avatar: ''
+          })
+        }
+      })
+
+    }
+
   }
-    
+}
+
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-
-</style>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
 
