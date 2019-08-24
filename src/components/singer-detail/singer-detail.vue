86d29d41af6fb39da41c52130b7cb3c87729d5fb
huqionglu <190109@lifeat.cn>
2019-08-24 18:39:58 +0800

    xiugai

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,18 +1,43 @@
 <template>
-	<div class="singer-detail">djkdsjfkdjfdksfkdsfdksj</div>
-		<!-- <transition name="slide"> -->
+
+		<transition name="slide">
+				<div class="singer-detail">djkdsjfkdjfdksfkdsfdksj</div>
 		  <!-- <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list> -->
-		<!-- </transition> -->
+		</transition>
 	  </template>
 	  
 	  <script>
 		import {ERR_OK} from 'api/config'
+		import {mapGetters} from 'vuex'
+
+		export default {
+			computed:{
+				...mapGetters([
+					'singer'
+				])
+			},
+			created() {
+				console.log(this.singer)
+			}
+		}
 	  </script>
 	  
 	  <style scoped lang="stylus" rel="stylesheet/stylus">
+	  @import "~common/stylus/variable"
+	
 		.slide-enter-active, .slide-leave-active
 		  transition: all 0.3s
 	  
 		.slide-enter, .slide-leave-to
 		  transform: translate3d(100%, 0, 0)
+
+		.singer-detail
+		  position:fixed
+		  top:0
+		  left:0
+		  right:0
+		  bottom:0
+		  z-index:100
+		  background:$color-background
+		  
 	  </style>
