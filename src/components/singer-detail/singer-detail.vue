72e83d585dc1ccf76ac8a146285aa017760adc39
huqionglu <190109@lifeat.cn>
2019-09-07 20:08:31 +0800

    tijiao2

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,23 +1,68 @@
 <template>
 
 		<transition name="slide">
-				<div class="singer-detail">djkdsjfkdjfdksfkdsfdksj</div>
-		  <!-- <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list> -->
+				<!-- <div class="singer-detail">djkdsjfkdjfdksfkdsfdksj</div> -->
+		  <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
 		</transition>
 	  </template>
 	  
 	  <script>
 		import {ERR_OK} from 'api/config'
 		import {mapGetters} from 'vuex'
-
+		import {getSingerDetail} from 'api/singer'
+		  import {createSong} from 'common/js/song'
+		import MusicList from 'components/music-list/music-list'
 		export default {
 			computed:{
+				title() {
+					return this.singer.name
+				},
+				bgImage() {
+					return this.singer.avatar
+				},
 				...mapGetters([
 					'singer'
 				])
 			},
 			created() {
+				this._getDetail()
 				console.log(this.singer)
+			},
+			   data() {
+				return {
+					songs: []
+				}
+			   },
+			methods:{
+			
+				_getDetail() {
+				   if (!this.singer.id) {
+					    this.$router.back()
+					   return
+				    }
+					getSingerDetail(this.singer.id).then(res => {
+						if (res.code === ERR_OK) {
+							
+							 this.songs = this._normalizeSongs(res.data.list)
+							 console.log( this.songs)
+						}
+					})
+
+				},
+				      _normalizeSongs(list) {
+						let ret = []
+						list.forEach((item) => {
+						let {musicData} = item
+						if (musicData.songid && musicData.albummid) {
+							ret.push(createSong(musicData))
+						}
+						})
+						return ret
+					}
+				
+			},
+			components:{
+				 MusicList
 			}
 		}
 	  </script>
